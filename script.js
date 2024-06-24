15d9b8c90cd048a425c1c1de5ae719c3f21415c8
Julianalanconi04 <149087940+Julianalanconi04@users.noreply.github.com>
2024-06-24 08:53:55 -0300

    24/06

--- script.js
+++ script.js
@@ -6,46 +6,58 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: Meio ambiente 
+        enunciado: "Pergunta 1",
         alternativas: [
-            "Alternativa 1"
-            "Alternativa 2"  
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
     {
-                enunciado: "Pergunta 2",
-                alternativas: [
-                    "Alternativa 1",
-                    "Alternativa 2"
-                ]
-            }
-{
-                enunciado: "Pergunta 1",
-                alternativas: [
-                    "Alternativa 1",
-                    "Aternativa 2"
-                ]
-            }
-{
-                enunciado: "Pergunta 1",
-                alternativas: [
-                    "Alternativa 1",
-                    "Aternativa 2"
-                ]
-            }
-{
-                enunciado: "Pergunta 1",
-                alternativas: [
-                    "Alternativa 1",
-                    "Aternativa 2"
-                ]
-            }
-
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },{
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+]
 let atual = 0;
-        let perguntaAtual;
+let perguntaAtual;
 
-        function mostraPergunta() {
-        perguntaAtual = perguntas[atual];
-        caixaPerguntas.textContent = perguntaAtual.enunciado;
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
 
-mostraPergunta()
+mostraPergunta();
+
+
+
 
